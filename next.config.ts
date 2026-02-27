2496fd6d2472fa2ac2c70740bb480466e8b8118e
HaddadMedAyoub <mohamedayoub.haddad@esprit.tn>
2026-02-27 16:51:49 +0100

    netlify fix

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/usbg",
-  assetPrefix: "/usbg/",
   images: {
     unoptimized: true,
   },
